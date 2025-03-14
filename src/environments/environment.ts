ad8a8e5125b6e806943562b83f89addcdb87f6c8
Lucas <lucasrasecc@gmail.com>
2025-03-14 19:27:37 -0300

    feat:removing SSR

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000' // URL local do JSON Server
+  apiUrl: 'http://localhost:3000/products' // URL local do JSON Server
 };
